39960e34fbaee130cc499454bde199cb5cae3f1c
flythecoop <website@flythe.coop>
2010-09-18 23:17:21 +0100

    changed root

--- config/routes.rb
+++ config/routes.rb
@@ -56,6 +56,7 @@ Traffordcounciltax::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+  root :to => "taxbands#index"
 
   # See how all your routes lay out with "rake routes"
 
